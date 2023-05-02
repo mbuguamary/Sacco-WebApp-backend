ea695f84967acf16e94b84b5aaaecbe3950a0b32
Mary Mbugua <mbuguamels@gmail.com>
2023-05-02 10:04:41 +0300

    created the dividend repository

---
+++ src/main/java/com/sacco/saccoapp/dividend/DividendRepository.java
@@ -0,0 +1,12 @@
+package com.sacco.saccoapp.dividend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface DividendRepository extends JpaRepository<Dividend,Integer> {
+    @Query("SELECT s from Dividend s where s.id =?1")
+    Optional<Dividend> findById(Long id);
+}
