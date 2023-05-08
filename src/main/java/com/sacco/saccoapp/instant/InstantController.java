c6be7ff76ea3ac8f679c4e1dff3bf8968067fa35
Mary Mbugua <mbuguamels@gmail.com>
2023-05-08 12:31:59 +0300

    created the instant controller

---
+++ src/main/java/com/sacco/saccoapp/instant/InstantController.java
@@ -0,0 +1,25 @@
+package com.sacco.saccoapp.instant;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RequestMapping(path="/api/v1/instant")
+public class InstantController {
+    @GetMapping
+    public List<Instant> getInstant(){
+        return instantService.getInstant();
+    }
+    @PostMapping(path="/register")
+
+    public void registerInstant (@RequestBody Instant instant){
+        instantService.addNewInstant(instant);
+
+    }
+
+}
