5917408127c0c4586dfd03a7ad18e5d832dc9bba
Mary Mbugua <mbuguamels@gmail.com>
2023-05-08 12:35:06 +0300

    created instant service

--- src/main/java/com/sacco/saccoapp/instant/InstantController.java
+++ src/main/java/com/sacco/saccoapp/instant/InstantController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping(path="/api/v1/instant")
 public class InstantController {
+    private final InstantService instantService;
     @GetMapping
     public List<Instant> getInstant(){
         return instantService.getInstant();
---
+++ src/main/java/com/sacco/saccoapp/instant/InstantService.java
@@ -0,0 +1,15 @@
+package com.sacco.saccoapp.instant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InstantService {
+    public List<Instant> getInstant() {
+
+    }
+
+    public void addNewInstant(Instant instant) {
+    }
+}
