cfb3aabf5972063dc6e007920d2c14f5722f74e6
Mary Mbugua <mbuguamels@gmail.com>
2023-05-12 15:15:30 +0300

    created guarantor service

--- src/main/java/com/sacco/saccoapp/guarantor/GuarantorController.java
+++ src/main/java/com/sacco/saccoapp/guarantor/GuarantorController.java
@@ -1,15 +1,18 @@
 package com.sacco.saccoapp.guarantor;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @RequestMapping(path = "/api/v1/guarantor")
+@AllArgsConstructor
 public class GuarantorController {
+    private final GuarantorService guarantorService;
     @GetMapping
     public List<Guarantor> getGuarantors(){
-        return guarantorService.getGuarantors;
+        return guarantorService.getGuarantors();
 
     }
 
---
+++ src/main/java/com/sacco/saccoapp/guarantor/GuarantorService.java
@@ -0,0 +1,14 @@
+package com.sacco.saccoapp.guarantor;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class GuarantorService {
+
+    public List<Guarantor> getGuarantors() {
+    }
+}
